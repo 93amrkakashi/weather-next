91cbf23156743f19da58101feeda4164561bac87
93amrkakashi <93amr.kakashi@gmail.com>
2023-12-18 14:59:24 -0800

    url for server

--- js/assits.js
+++ js/assits.js
@@ -1,5 +1,5 @@
 loadLayout()
-const url = `http://localhost:5000/api/`
+const url = `https://weather-server-gules.vercel.app/api/`
 const stateNames = Object.keys(citiesData);
 const citiesArray = stateNames;
 const selectElement = document.getElementById("languageSelect");
